05f2d04dea91938700a33374b4b650625bb7545b
mariomartin <mariomartingonz@gmail.com>
2024-02-15 23:03:20 +0100

    formulario

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { useState } from "react";
 import { Home } from './components/Pages/Home/Home';
 import { SeccionMapa } from './components/Pages/SeccionMapa/SeccionMapa';
 import { SeccionAnimales } from './components/Pages/SeccionAnimales/SeccionAnimales';
 import { SeccionPerfil } from './components/Pages/SeccionPerfil/SeccionPerfil';
 import { SeccionOtrosServicios } from './components/Pages/SeccionOtrosServicios/SeccionOtrosServicios';
+import { Formulario } from './components/Formulario/Formulario';
+import { Formulario2 } from './components/Formulario/Formulario2';
+import { Formulario3 } from './components/Formulario/Formulario3';
 
 function App() {
+
   return (
     <>
       <BrowserRouter>
@@ -16,6 +21,9 @@ function App() {
         <Route path = "/Adopcion" element= {<SeccionAnimales/>} />
         <Route path = "/Perfil" element= {<SeccionPerfil/>} />
         <Route path = "/OtrosServicios" element= {<SeccionOtrosServicios/>} />
+        <Route path = "/Formulario" element= {<Formulario/>}/> 
+        <Route path = "/Formulario2" element= {<Formulario2/>}/>
+        <Route path = "/Formulario3" element= {<Formulario3/>}/>
         </Routes> 
       </BrowserRouter>
          </>
--- src/components/Formulario/Formulario.jsx
+++ src/components/Formulario/Formulario.jsx
@@ -132,3 +132,6 @@ export const Formulario = ({statusAdop}) => {
 };
 
 export default Formulario;
+
+
+//Comentario para pushear
